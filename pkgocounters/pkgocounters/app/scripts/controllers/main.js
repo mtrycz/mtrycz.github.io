7791ded1558c2cc28f8d68f41378ba7728cc3fb5
MarcinTrycz <foma84@yahoo.it>
2016-09-17 00:37:05 +0200

    Primo funzionamento di tabella

--- pkgocounters/pkgocounters/app/scripts/controllers/main.js
+++ pkgocounters/pkgocounters/app/scripts/controllers/main.js
@@ -8,21 +8,22 @@
  * Controller of the pkgocountersApp
  */
 angular.module('pkgocountersApp', ['datatables'])
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  }).controller('WithAjaxCtrl', WithAjaxCtrl);
+.controller('MainCtrl', function () {
+	this.awesomeThings = [
+	                      'HTML5 Boilerplate',
+	                      'AngularJS',
+	                      'Karma'
+	                      ];
+}).controller('WithAjaxCtrl', WithAjaxCtrl);
 
 function WithAjaxCtrl(DTOptionsBuilder, DTColumnBuilder) {
-    var vm = this;
-    vm.dtOptions = DTOptionsBuilder.fromSource('json/defenders.json')
-        .withPaginationType('full_numbers');
-    vm.dtColumns = [
-        DTColumnBuilder.newColumn('defendername').withTitle('defendername'),
-        DTColumnBuilder.newColumn('defenderfastmove').withTitle('defenderfastmove'),
-        DTColumnBuilder.newColumn('defenderspecialmove').withTitle('defenderspecialmove')
-    ];
+	var vm = this;
+
+//	vm.dtOptions = DTOptionsBuilder.fromSource('json/defenders.json').withScroller();
+//	vm.dtColumns = [
+//	DTColumnBuilder.newColumn('defendername').withTitle('defendername').notSortable(),
+//	DTColumnBuilder.newColumn('defenderfastmove').withTitle('defenderfastmove').notSortable(),
+//	DTColumnBuilder.newColumn('defenderspecialmove').withTitle('defenderspecialmove').notSortable(),
+//	DTColumnBuilder.newColumn('won').withTitle('won')
+//	];
 }
