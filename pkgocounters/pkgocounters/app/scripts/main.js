717e4fc6be53094f2d1d1e346efd9cfb7ffed38f
Marcin I. Trycz <foma84@yahoo.it>
2017-02-18 13:56:09 +0100

    typos

--- pkgocounters/pkgocounters/app/scripts/main.js
+++ pkgocounters/pkgocounters/app/scripts/main.js
@@ -115,7 +115,7 @@ $(document).ready(function(){
 //	defenders.column(4).search("Yes");
 	//defenders.column(1).search("Zen");
 
-	defenders2 = $('#table_defenders2').DataTable({
+	var defenders2 = $('#table_defenders2').DataTable({
 		ajax: {
 			url: 'json/empty.json',
 			dataSrc: ''
